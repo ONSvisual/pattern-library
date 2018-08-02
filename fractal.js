011e847c88dbdfcf268651be3e4380ab439e8f66
Benjy Stanton <benjystanton@gmail.com>
2018-08-02 11:56:20 +0100

    Statuses

--- fractal.js
+++ fractal.js
@@ -29,3 +29,29 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
  * Tell the Fractal web preview plugin where to look for static assets.
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+/*
+* Custom status types for components
+*/
+fractal.components.set('statuses', {
+  inDevelopment: {
+    label: 'In development',
+    description: 'Do not use.',
+    color: '#D32F2F'
+  },
+  testing: {
+    label: 'Testing',
+    description: 'Use with caution.',
+    color: '#FF9933'
+  },
+  ready: {
+    label: 'Ready',
+    description: 'Go! Go! Go!',
+    color: '#0F8243'
+  },
+  notInUse: {
+    label: 'Not in use',
+    description: 'May day',
+    color: '#D0D2D3'
+  }
+});
