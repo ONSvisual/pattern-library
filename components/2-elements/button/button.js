1f56b8ec10366e3a2b4af42a4affe722b3ec3964
Joe <joseph.nation@ons.gov.uk>
2018-10-17 08:30:09 +0100

    Updated PL

---
+++ components/2-elements/button/button.js
@@ -0,0 +1,19 @@
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+$(function () {
+    //show
+    $(document).on('click', '.trigger', function () {
+        $(this).addClass("on");
+        setTimeout(RemoveClass, 2000);
+		});
+		function RemoveClass() {
+		$('.trigger').removeClass("on");
+		}
+		});
