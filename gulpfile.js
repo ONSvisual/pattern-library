8a033b1dbba3b6e2cd36533c7debf217bc47cba2
Joe <joseph.nation@ons.gov.uk>
2018-10-22 11:38:56 +0100

    Updated PL
    Many many files have updated. Here is the latest

--- gulpfile.js
+++ gulpfile.js
@@ -44,3 +44,99 @@ gulp.task('fractal:start', function(){
 });
 
 gulp.task('default', ['fractal:start', 'sass', 'watch']);
+
+/* Scripts */
+
+gulp.task('scripts:clean', function() {
+  return del(['public/assets/scripts']);
+});
+
+const bundleScripts = watch => {
+  let cache;
+
+  var bundler = function() {
+    var b = browserify()
+      .transform('rollupify', {
+        config: {
+          onwarn: function(message) {
+            if (message.code === 'THIS_IS_UNDEFINED') {
+              return;
+            }
+
+            console.error(message);
+          },
+
+          plugins: [
+            nodeResolve({
+              jsnext: true,
+              main: true,
+              preferBuiltins: false
+            }),
+            commonjs({
+              include: 'node_modules/**',
+              namedExports: {
+                'node_modules/events/events.js': Object.keys(require('events'))
+              }
+            }),
+            rollupBabel({
+              plugins: ['lodash'],
+              presets: ['es2015-rollup', 'stage-2'],
+              babelrc: false,
+              exclude: 'node_modules/!**'
+            })
+          ]
+        },
+      }),
+      stream = through2.obj(function(file, enc, next) {
+        // add each file to the bundle
+        b.add(file.path);
+        next();
+      }, function(next) {
+        b.bundle(function(err, src) {
+          if(err) {
+            throw err;
+          }
+
+          // create a new vinyl file with bundle contents
+          // and push it to the stream
+          stream.push(new vinyl({
+            path: 'bundle.js', // this path is relative to dest path
+            contents: src
+          }));
+          next();
+        });
+      });
+    return stream;
+  };
+
+  return gulp.src(['./assets/js/polyfills.js', './assets/js/components.js'])
+    .pipe(bundler())
+    .on('error', function(err) {
+      gutil.log(err.message);
+      this.emit('end');
+    })
+    .pipe(gulp.dest('public/assets/scripts'));
+};
+
+gulp.task('scripts:bundle', () => bundleScripts(false));
+
+/* UGLIFY of bundled scripts
+    - run this after all scripts are bundled to produce a minified version
+*/
+gulp.task('scripts:uglify', function(done) {
+  return bundleScripts(false)
+    .on('error', function(err) {
+      gutil.log(err.message);
+      this.emit('end');
+    })
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/assets/scripts'));
+});
+/* END UGLIFY of bundled scripts */
