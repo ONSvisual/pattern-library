9302bfd4920528b67a613915830c492f6bfc25fd
Joe Nation <joseph.nation@ons.gov.uk>
2019-04-17 08:42:24 +0100

    Last push request

---
+++ components/2-elements/slider/slider.config.js
@@ -0,0 +1,19 @@
+
+  module.exports = {
+    'title': 'Buttons',
+  'default': 'primary',
+  'collated': true,
+  'status': 'inDevelopment',
+  'variants': [{
+    'name': 'primary',
+    'label': 'Primary',
+    'context': {
+      'label': 'Primary button',
+      'btn-classes': 'btn--primary'
+    },
+  },{
+    'name': 'simple',
+    'label': 'Simple'
+
+  },
+  }
